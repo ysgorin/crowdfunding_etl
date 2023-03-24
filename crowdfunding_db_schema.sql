299ccd5806602617b3cad8ca158c1d08d914c205
ysgorin <ysgorin@gmail.com>
2023-03-23 21:58:15 -0400

    edited table schemata

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -29,6 +29,7 @@ CREATE TABLE Campaign (
     description VARCHAR NOT NULL,
     goal FLOAT NOT NULL,
     pledged FLOAT NOT NULL,
+    outcome VARCHAR NOT NULL,
     backers_count INT NOT NULL,
     country CHAR(2) NOT NULL,
     currency CHAR(3) NOT NULL,
